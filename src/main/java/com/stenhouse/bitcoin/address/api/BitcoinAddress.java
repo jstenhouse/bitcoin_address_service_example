328c5bf588e5db14e855495482beb87ed08af4f5
Jason Stenhouse <jason.stenhouse@gmail.com>
2015-12-24 21:32:34 +0100

    adding pseudo random generation of bitcoin addresses

--- src/main/java/com/stenhouse/bitcoin/address/api/BitcoinAddress.java
+++ src/main/java/com/stenhouse/bitcoin/address/api/BitcoinAddress.java
@@ -1,14 +1,18 @@
 package com.stenhouse.bitcoin.address.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.subgraph.orchid.encoders.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.bitcoinj.core.Address;
+import org.bitcoinj.params.MainNetParams;
 import org.hibernate.validator.constraints.NotEmpty;
+
+import java.util.Random;
 
 /**
  * Created by jason on 12/24/15.
  */
 public class BitcoinAddress {
-
-    // ex. 3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy
 
     @NotEmpty
     private String address;
@@ -17,8 +21,8 @@ public class BitcoinAddress {
         // Jackson deserialization
     }
 
-    public BitcoinAddress(final String address) {
-        this.address = address;
+    public BitcoinAddress(final String value) {
+        this.address = new Address(MainNetParams.get(), Hex.decode(value)).toString();
     }
 
     @JsonProperty
@@ -26,7 +30,23 @@ public class BitcoinAddress {
         return address;
     }
 
+
     public static BitcoinAddress nextRandom() {
-        return new BitcoinAddress("3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy");
+        // should be RIPEMD160 but we'll cheat a little
+        final String randomSHA40 = nextRandomSHA40();
+
+        return new BitcoinAddress(randomSHA40);
+    }
+
+    private static String nextRandomSHA40() {
+        final String sha = DigestUtils.sha256Hex(nextRandomBytes());
+
+        return sha.substring(0, 40);
+    }
+
+    private static byte[] nextRandomBytes() {
+        byte[] bytes = new byte[50];
+        new Random().nextBytes(bytes);
+        return bytes;
     }
 }
